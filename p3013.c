8049e174e08bcf737fe6ed6fcd520c49627b013c
ZXC <zhxxch@outlook.com>
2020-11-20 16:28:43 +0800

    primitive root 5 for 2^30+2^13+1

--- p3013.c
+++ p3013.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "speck.h"
+const int p3013_factors[] = {2, 3, 43691};
+const int p3013_orders[] = {0x40002000 / 2,
+	0x40002000 / 3, 0x40002000 / 43691};
 inline int32_t mod_p3013(const uint64_t v) {
 	const uint64_t f1 = v & (0x3ffull << 51);
 	const uint64_t m1 = f1 + (f1 >> 17) + (f1 >> 30);
@@ -33,4 +37,30 @@ int test_modp3013(const int Num, const int Nonce) {
 		(ans == res ? "Eq" : "Neq"), ans, res);
 	return test_modp3013(Num - 1, Nonce + 100001);
 }
-int main(void) { return test_modp3013(10, 2222); }
+uint64_t pow_Zp_ring(const uint64_t p,
+	const uint64_t base, const uint64_t exponent) {
+	uint64_t ans = 1;
+	for(uint64_t exp = exponent, g = base; exp;) {
+		if(exp & 1) { ans = (ans * g) % p; }
+		exp = (exp >> 1);
+		g = (g * g) % p;
+	}
+	return ans;
+}
+bool is_p3013_gen(const int32_t a) {
+	for(int i = 0; i < 3; i++) {
+		const uint64_t r = pow_Zp_ring(
+			0x40002001, a, p3013_orders[i]);
+		if(r <= 1) return false;
+	}
+	return true;
+}
+int search_p3013_gens(const int Num) {
+	for(int i = 0; i < Num; i++) {
+		if(is_p3013_gen(i)) {
+			printf("Generator: %i\n", i);
+		}
+	}
+	return 0;
+}
+int main(void) { return search_p3013_gens(100); }
