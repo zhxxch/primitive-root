64fd8e4807540a4672fb588bff9e4ee1318c51a0
ZXC <zhxxch@outlook.com>
2021-02-14 23:41:25 +0800

    using f64

--- primeq.c
+++ primeq.c
@@ -4,13 +4,13 @@
 #include <stdint.h>
 int main(int ac, char *av[]) {
 	if(ac <= 1) { return 1; }
-	const uint64_t N = strtoll(av[1], NULL, 10);
+	const double N = strtod(av[1], NULL);
 	if(ac <= 2) {
 		const int prime_bit = primeq(N);
 		printf("%i\n", prime_bit);
 		return prime_bit;
 	}
-	const uint64_t N1 = strtoll(av[2], NULL, 10);
+	const double N1 = strtod(av[2], NULL);
 	if(ac <= 3) {
 		for(uint64_t i = N; i < N1; i++) {
 			if(primeq(i)) { printf("%lli\n", i); }
--- primeq.h
+++ primeq.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdio.h>
+#include <math.h>
 inline double f64high24bit(
 	const double x, const double scalar) {
 	const double d = scalar * (1 << 24);
@@ -42,67 +42,73 @@ inline double f48_mul_mod(const double a,
 		= a_lo * b_lo - scalar_lo * mod_lo;
 	return diff_hi + diff_m1 + diff_m2 + diff_lo;
 }
-void primeq_rabin_miller(const long long n,
+double remove_factor2(const double x) {
+	for(double hx = x;;) {
+		const double hhx = hx / 2;
+		if(floor(hhx) != hhx) return hx;
+		hx = hhx;
+	}
+}
+void primeq_rabin_miller(const double n,
 	double bases[], double answs[],
 	const int ans_len) {
+	const double d = remove_factor2(n - 1);
+	const double s = (n - 1) / d;
 	typedef long long i64;
-	const i64 m = n - 1;
-	const i64 s = m & (~m + 1);
-	const i64 d = m / s;
-	const double nn = (double)n;
-	const double nn_hi = f64high24bit(nn, 1);
-	const double nn_lo = nn - nn_hi;
+	const double n_hi = f64high24bit(n, 1);
+	const double n_lo = n - n_hi;
 	for(int i = 0; i < ans_len; i++) { answs[i] = 1; }
-	for(i64 exp = d; exp; exp /= 2) {
+	for(i64 exp = (i64)d; exp; exp /= 2) {
 		if(exp & 1) {
 			for(int i = 0; i < ans_len; i++) {
 				answs[i] = f48_mul_mod(
-					answs[i], bases[i], nn_hi, nn_lo);
+					answs[i], bases[i], n_hi, n_lo);
 			}
 			for(int i = 0; i < ans_len; i++) {
 				answs[i]
-					= (answs[i] < 0 ? (answs[i] + nn)
+					= (answs[i] < 0 ? (answs[i] + n)
 									: answs[i]);
 			}
 		}
 		for(int i = 0; i < ans_len; i++) {
 			bases[i] = f48_mul_mod(
-				bases[i], bases[i], nn_hi, nn_lo);
+				bases[i], bases[i], n_hi, n_lo);
 		}
 		for(int i = 0; i < ans_len; i++) {
-			bases[i] = (bases[i] < 0 ? (bases[i] + nn)
+			bases[i] = (bases[i] < 0 ? (bases[i] + n)
 									 : bases[i]);
 		}
 	}
 	for(int i = 0; i < ans_len; i++) {
 		bases[i] = ((answs[i] == 1) ? 0 : answs[i]);
 	}
-	for(i64 k = s; k; k >>= 1) {
+	for(i64 k = (i64)s; k; k >>= 1) {
 		for(int i = 0; i < ans_len; i++) {
-			answs[i] = (bases[i] == m ? 1 : answs[i]);
+			answs[i]
+				= (bases[i] == (n - 1) ? 1 : answs[i]);
 		}
 		for(int i = 0; i < ans_len; i++) {
 			bases[i] = f48_mul_mod(
-				bases[i], bases[i], nn_hi, nn_lo);
+				bases[i], bases[i], n_hi, n_lo);
 		}
 		for(int i = 0; i < ans_len; i++) {
-			bases[i] = (bases[i] < 0 ? (bases[i] + nn)
+			bases[i] = (bases[i] < 0 ? (bases[i] + n)
 									 : bases[i]);
 		}
 	}
 }
 
-int primeq(const long long n) {
+int primeq(const double n) {
 	/* Ref: https://primes.utm.edu/prove/prove2_3.html */
 	double Primes[] = {2, 3, 5, 7, 11, 13, 17, 19};
 	double Ans[sizeof(Primes) / sizeof(double)];
-	if(n <= 1) return 0;
+	if(n <= 1 || n >= (1ll << 48)) return 0;
 	for(int i = 0; i < sizeof(Primes) / sizeof(double);
 		i++) {
 		if(n == Primes[i]) return (1);
 	}
-	primeq_rabin_miller(n & ((1ll << 48) - 1), Primes,
-		Ans, sizeof(Ans) / sizeof(double));
+	primeq_rabin_miller(
+		n, Primes, Ans, sizeof(Ans) / sizeof(double));
 	for(int i = 0; i < sizeof(Ans) / sizeof(double);
 		i++) {
 		if(Ans[i] != 1) return 0;
